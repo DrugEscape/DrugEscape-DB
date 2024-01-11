4d41a08dd7fd83962e4f50fd0ee3870ba61f4ba8
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-01-11 15:16:59 +0900

    DB version 2.1

---
+++ drugescape.sql
@@ -0,0 +1,32 @@
+CREATE DATABASE drugescape;
+use drugescape;
+
+CREATE TABLE member (
+id INTEGER PRIMARY KEY ,
+name VARCHAR(255),
+email VARCHAR(255),
+picture VARCHAR(255),
+role VARCHAR(255));
+
+CREATE TABLE report (
+id  INTEGER PRIMARY KEY,
+point INTEGER,
+accumulated_days INTEGER,
+maximum_days INTEGER,
+daily_goals INTEGER);
+
+CREATE TABLE management (
+id INTEGER PRIMARY KEY ,
+stop_drug VARCHAR(255),
+exercise VARCHAR(255),
+meal VARCHAR(255),
+medication VARCHAR(255),
+report_id INTEGER,
+FOREIGN KEY(report_id) REFERENCES report(id)
+);
+
+CREATE TABLE donation (
+id  INTEGER PRIMARY KEY,
+donated_point INTEGER,
+report_id INTEGER,
+FOREIGN KEY(report_id) REFERENCES report(id));
