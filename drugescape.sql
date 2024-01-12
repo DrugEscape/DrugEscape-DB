499e2b314405bb1c2dd30d7f946d20e9363bd13f
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-01-13 03:13:15 +0900

    DB 자료형 및 변수 이름 변경

--- drugescape.sql
+++ drugescape.sql
@@ -1,33 +1,36 @@
 CREATE DATABASE drugescape;
-use drugescape;
+USE drugescape;
 
 CREATE TABLE member (
-id INTEGER PRIMARY KEY ,
-name VARCHAR(255),
-email VARCHAR(255),
-picture VARCHAR(255),
-role VARCHAR(255));
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255),
+    email VARCHAR(255),
+    picture VARCHAR(255),
+    role VARCHAR(255)
+);
 
 CREATE TABLE report (
-id  INTEGER PRIMARY KEY,
-point INTEGER,
-accumulated_days INTEGER,
-maximum_days INTEGER,
-daily_goals INTEGER);
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    point INT,
+    accumulated_days INT,
+    maximum_days INT,
+    daily_goals INT
+);
 
 CREATE TABLE management (
-id INTEGER PRIMARY KEY ,
-stop_drug VARCHAR(255),
-exercise VARCHAR(255),
-meal VARCHAR(255),
-medication VARCHAR(255),
-report_id INTEGER,
-FOREIGN KEY(report_id) REFERENCES report(id)
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    stop_drug VARCHAR(255),
+    exercise VARCHAR(255),
+    meal VARCHAR(255),
+    medication VARCHAR(255),
+    report_id BIGINT,
+    FOREIGN KEY(report_id) REFERENCES report(id)
 );
 
 CREATE TABLE donation (
-id  INTEGER PRIMARY KEY,
-donating_point INTEGER,
-donated_point INTEGER,
-report_id INTEGER,
-FOREIGN KEY(report_id) REFERENCES report(id));
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    donating_point INT,
+    donated_point INT,
+    report_id BIGINT,
+    FOREIGN KEY(report_id) REFERENCES report(id)
+);
