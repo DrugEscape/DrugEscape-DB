d00e46714115f9d2873e923e71d06fe630e32859
최민우 <112475136+chaiminwoo0223@users.noreply.github.com>
2024-01-13 18:15:54 +0900

    게시판 기능 추가

--- drugescape.sql
+++ drugescape.sql
@@ -12,25 +12,58 @@ CREATE TABLE member (
 CREATE TABLE report (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
     point INT,
-    accumulated_days INT,
-    maximum_days INT,
-    daily_goals INT
+    accumulatedDays INT,
+    maximumDays INT,
+    dailyGoals INT
 );
 
 CREATE TABLE management (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    stop_drug VARCHAR(255),
+    stopDrug VARCHAR(255),
     exercise VARCHAR(255),
     meal VARCHAR(255),
     medication VARCHAR(255),
-    report_id BIGINT,
-    FOREIGN KEY(report_id) REFERENCES report(id)
+    reportId BIGINT,
+    FOREIGN KEY(reportId) REFERENCES report(id)
 );
 
 CREATE TABLE donation (
     id BIGINT AUTO_INCREMENT PRIMARY KEY,
-    donating_point INT,
-    donated_point INT,
-    report_id BIGINT,
-    FOREIGN KEY(report_id) REFERENCES report(id)
+    donatingPoint INT,
+    donatedPoint INT,
+    reportId BIGINT,
+    FOREIGN KEY(reportId) REFERENCES report(id)
+);
+
+CREATE TABLE board (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255),
+    content VARCHAR(255),
+    image VARCHAR(255),
+    likeCount INT,
+    viewCount INT,
+    commentCount INT,
+    createdAt datetime,
+    modifiedAt datetime,
+    memberId BIGINT,
+    FOREIGN KEY(memberId) REFERENCES member(id)
+);
+
+CREATE TABLE comment (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(255),
+    createdAt datetime,
+    modifiedAt datetime,
+    memberId BIGINT,
+    boardId BIGINT,
+    FOREIGN KEY(memberId) REFERENCES member(id),
+    FOREIGN KEY(boardId) REFERENCES board(id)
+);
+
+CREATE TABLE heart (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    memberId BIGINT,
+    boardId BIGINT,
+    FOREIGN KEY(memberId) REFERENCES member(id),
+    FOREIGN KEY(boardId) REFERENCES board(id)
 );
